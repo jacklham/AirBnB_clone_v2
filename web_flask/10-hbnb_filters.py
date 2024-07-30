e113a4bb861426035bfe1d278ec47c24ce2cd96f
kuelmazier <rremeeniz@gmail.com>
2024-07-31 01:55:17 +0300

    adding files

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,27 +1,29 @@
 #!/usr/bin/python3
-"""
-start Flask application
-"""
+"""Start a Flask web application"""
 
 from flask import Flask, render_template
-from models import *
 from models import storage
+from models.state import State
+from models.amenity import Amenity
+
+
 app = Flask(__name__)
 
 
-@app.route('/hbnb_filters', strict_slashes=False)
-def filters():
-    """display a HTML page like 6-index.html from static"""
-    states = storage.all("State").values()
-    amenities = storage.all("Amenity").values()
-    return render_template('10-hbnb_filters.html', states=states,
-                           amenities=amenities)
-
-
 @app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
+def teardown(self):
+    """removes the current SQLAlchemy Session"""
     storage.close()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def hbnb_filters():
+    """Displays HBnB filters page"""
+    states = storage.all(State)
+    amenities = storage.all(Amenity)
+    return render_template("10-hbnb_filters.html",
+                           states=states, amenities=amenities)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,35 +1,42 @@
 #!/usr/bin/python3
-"""
-start Flask application
-"""
+"""Start a Flask web applicaton"""
 
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def index():
-    """returns Hello HBNB!"""
-    return 'Hello HBNB!'
+def hello_hbnb():
+    """
+    Routing to root, strict_slashes ensure
+    the URL works when it ends both with or without the /
+    """
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """returns HBNB"""
-    return 'HBNB'
+    """
+    Routing to /hbnb, strict_slashes ensure
+    the URL works when it ends both with or without the /
+    """
+    return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def cisfun(text):
-    """display “C ” followed by the value of the text variable"""
-    return 'C ' + text.replace('_', ' ')
+def c_is_fun(text):
+    """Routing to C using Variables"""
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
 
 
-@app.route('/python', strict_slashes=False)
+@app.route('/python/', defaults={'text': 'is_cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def pythoniscool(text='is cool'):
-    """display “Python ”, followed by the value of the text variable"""
-    return 'Python ' + text.replace('_', ' ')
+def python_is_cool(text):
+    """Routing to python with default value using Variables"""
+    text = text.replace('_', ' ')
+    return "Python {}".format(text)
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
